35cf9082062161200c200fb44a866914878680a6
kwangsun1999 <135393233+kwangsun1999@users.noreply.github.com>
2025-07-30 17:54:16 +0900

    commit: 혜택 페이지 작성

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+  server: {
+    proxy: {
+      // 💡 '/api'로 시작하는 모든 요청을 대상으로 프록시 설정
+      '/api': {
+        target: 'http://localhost:8080', // Spring 백엔드 서버 주소
+        changeOrigin: true,              // CORS 오류 방지를 위해 출처를 변경
+      }
+    }
+  },
+  
+});
