c7e5d94e59a16f9ca27a4ec618b423793f01cae7
cseongbo <cseongbo17@gmail.com>
2025-08-04 11:46:24 +0900

    ⚡️feat: 일반 로그인 및 네이버 소셜로그인 기능 추가 #14

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MainPage from '@/pages/MainPage.vue'
-import BookingPage from '@/pages/booking/BookingPage.vue'
-import MapPage from '@/pages/map/MapPage.vue'
-import MyPage from '@/pages/mypage/MyPage.vue'
-import AnalysisPage from '@/pages/analysis/AnalysisPage.vue'
-import NotFoundPage from '@/pages/common/NotFoundPage.vue'
-import BookingAccommodationPage from '@/pages/booking/BookingAccommodationPage.vue'
-import FinalBookingPage from '@/pages/booking/FinalBookingPage.vue'
-import PaymentPage from "@/pages/payment/PaymentPage.vue"
-import QRPage from '@/pages/payment/QRPage.vue'
-import LabPage from '@/pages/lab/LabPage.vue'
-import FortuneCard from '@/pages/lab/FortuneCard.vue'
-import LoginPage from '@/pages/login/LoginPage.vue'
-import JoinPage from '@/pages/login/JoinPage.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from '@/pages/MainPage.vue';
+import BookingPage from '@/pages/booking/BookingPage.vue';
+import MapPage from '@/pages/map/MapPage.vue';
+import MyPage from '@/pages/mypage/MyPage.vue';
+import AnalysisPage from '@/pages/analysis/AnalysisPage.vue';
+import NotFoundPage from '@/pages/common/NotFoundPage.vue';
+import BookingAccommodationPage from '@/pages/booking/BookingAccommodationPage.vue';
+import FinalBookingPage from '@/pages/booking/FinalBookingPage.vue';
+import PaymentPage from '@/pages/payment/PaymentPage.vue';
+import QRPage from '@/pages/payment/QRPage.vue';
+import LabPage from '@/pages/lab/LabPage.vue';
+import FortuneCard from '@/pages/lab/FortuneCard.vue';
+import LoginPage from '@/pages/login/LoginPage.vue';
+import JoinPage from '@/pages/login/JoinPage.vue';
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
+import NaverCallback from '@/pages/login/NaverCallback.vue';
 const routes = [
-  { path: '/', name: 'Main', component: MainPage },
+  {
+    path: '/',
+    name: 'Main',
+    component: MainPage,
+    meta: { requiresAuth: true },
+  },
   { path: '/booking', name: 'Booking', component: BookingPage },
-  { path: '/accommodation/:id', name: 'BookingAccommodationPage', component: BookingAccommodationPage },
-  { path: '/booking/confirm', name: 'FinalBookingPage', component: FinalBookingPage },
+  {
+    path: '/accommodation/:id',
+    name: 'BookingAccommodationPage',
+    component: BookingAccommodationPage,
+  },
+  {
+    path: '/booking/confirm',
+    name: 'FinalBookingPage',
+    component: FinalBookingPage,
+  },
   { path: '/map', name: 'Map', component: MapPage },
   { path: '/analysis', name: 'Analysis', component: AnalysisPage },
   { path: '/mypage', name: 'MyPage', component: MyPage },
@@ -30,20 +45,35 @@ const routes = [
   {
     path: '/payment/qr',
     component: QRPage,
-    meta: { hideNav: true }
+    meta: { hideNav: true },
   },
   {
     path: '/notification',
     name: 'Notification',
     component: () => import('@/pages/notification/NotificationPage.vue'),
-    meta: { hideHeader: true }
+    meta: { hideHeader: true },
   },
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage }
-]
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage },
+  {
+    path: '/naver/callback',
+    name: 'NaverCallback',
+    component: NaverCallback,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.isLogin) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
