9b19950382c8bec6a21d8072d01b9caf203c0c3f
cseongbo <cseongbo17@gmail.com>
2025-08-06 09:40:13 +0900

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    'proxy': {
-      'api': 'http://localhost:8080',
-      changeOrigin: true,
-    }
-  },
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청을 대상 서버로 전달
+      // '/api'로 시작하는 요청을 대상 서버로 전달
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // /api 유지
+      },
+    },
+  },
+});
